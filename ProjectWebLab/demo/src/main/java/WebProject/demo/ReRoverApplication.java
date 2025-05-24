1ea62c5ec785d2b9e48dc717c51a202be379ca93
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2025-05-24 20:56:17 +0700

    add

---
+++ ProjectWebLab/demo/src/main/java/WebProject/demo/ReRoverApplication.java
@@ -0,0 +1,13 @@
+package WebProject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReRoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReRoverApplication.class, args);
+	}
+
+}
---
+++ ProjectWebLab/demo/src/test/java/WebProject/demo/ReRoverApplicationTests.java
@@ -0,0 +1,13 @@
+package WebProject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReRoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
