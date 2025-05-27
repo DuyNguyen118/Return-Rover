06976eb6ca0859ea4e86b79171aa1aea6f7e9e22
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2025-05-27 16:31:10 +0700

    feat: add webconfig to interact with frontend

---
+++ ReRover/src/main/java/WebProject/ReRover/config/WebConfig.java
@@ -0,0 +1,16 @@
+package WebProject.ReRover.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Your Node.js frontend
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
