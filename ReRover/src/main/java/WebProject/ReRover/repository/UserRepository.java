4fdb7fb0269d7dd5b0abc5c1c26c9ebfcde8f4d1
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2025-05-28 21:46:47 +0700

    fix

--- ReRover/src/main/java/WebProject/ReRover/repository/UserRepository.java
+++ ReRover/src/main/java/WebProject/ReRover/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package WebProject.ReRover.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import WebProject.ReRover.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    
+    Optional<User> findByStudentId(String studentId);
 }
--- ReRover/src/main/java/WebProject/ReRover/services/Impl/UserServiceImpl.java
+++ ReRover/src/main/java/WebProject/ReRover/services/Impl/UserServiceImpl.java
@@ -4,15 +4,14 @@ import WebProject.ReRover.model.User;
 import WebProject.ReRover.repository.UserRepository;
 import WebProject.ReRover.services.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public User getUserById(int id) {
