e14b164c1e543ef7c2c32023b1530c610449d9a6
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2025-06-01 12:34:32 +0700

    feat: add method to update user profile

--- ReRover/src/main/java/WebProject/ReRover/services/Impl/UserServiceImpl.java
+++ ReRover/src/main/java/WebProject/ReRover/services/Impl/UserServiceImpl.java
@@ -19,6 +19,20 @@ public class UserServiceImpl implements UserService {
     public Optional<User> getUserByStudentId(String studentId) {
         return userRepository.findByStudentId(studentId);
     }   
+
+    @Override
+    public User updateUser(int id, User user) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            existingUser.setFullname(user.getFullname());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setStudentId(user.getStudentId());
+            existingUser.setPhoneNumber(user.getPhoneNumber());
+            existingUser.setProfilePicture(user.getProfilePicture());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
     
     @Override
     public Optional<User> getUserById(int id) {
