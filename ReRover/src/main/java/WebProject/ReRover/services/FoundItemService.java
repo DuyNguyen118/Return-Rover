6aa087617053911ad0ed6bcda81f6b8157674aeb
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2025-06-03 19:03:31 +0700

    feat: add

--- ReRover/src/main/java/WebProject/ReRover/services/FoundItemService.java
+++ ReRover/src/main/java/WebProject/ReRover/services/FoundItemService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public interface FoundItemService {
     public List<FoundItem> getAllFoundItems();
+    public List<FoundItem> getFoundItemsByUserId(int userId);
     public FoundItem getFoundItemById(int id);
     public FoundItem saveFoundItem(FoundItem foundItem);
     public void deleteFoundItem(int id);
--- ReRover/src/main/java/WebProject/ReRover/services/LostItemService.java
+++ ReRover/src/main/java/WebProject/ReRover/services/LostItemService.java
@@ -1,10 +1,12 @@
 package WebProject.ReRover.services;
 
-import WebProject.ReRover.model.LostItem;
 import java.util.List;
+
+import WebProject.ReRover.model.LostItem;
 
 public interface LostItemService {
     public List<LostItem> getAllLostItems();
+    public List<LostItem> getLostItemsByUserId(int userId);
     public LostItem getLostItemById(int id);
     public LostItem saveLostItem(LostItem lostItem);
     public void deleteLostItem(int id);
